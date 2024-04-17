8bb4463588e29f54deeb226283fd853c5dc7a830
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-04-17 20:02:52 +0300

    upd pacckage, pixabay

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '43441081-c9c9daac9af91d4227dda2db1';
+
+export async function objectSearch(text, page) {
+  const response = await axios(BASE_URL, {
+    params: {
+      key: KEY,
+      q: text,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: page,
+      per_page: 15,
+    },
+  });
+
+  return response.data;
+}
